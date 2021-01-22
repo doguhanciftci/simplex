bffbd83c445b9518022576c775de0cfb51e61176
Doğuhan Çiftçi <doguhan.ciftci@arcelik.com>
2021-01-22 23:21:07 +0300

    son değişiklikler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { AppBar, Button, Divider, Grid, InputAdornment, IconButton, Paper, Select, Table, MenuItem, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Card, CardContent, CardActions, Button, Divider, Grid, InputAdornment, IconButton, Paper, Select, Table, MenuItem, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Toolbar, Typography, CardHeader } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React, { useEffect, useState } from 'react';
-import { FaCalculator, FaEquals, FaGreaterThanEqual, FaLessThanEqual, FaPlus } from 'react-icons/fa';
+import { FaCalculator,FaPlayCircle, FaEquals, FaGreaterThanEqual, FaLessThanEqual, FaPlus } from 'react-icons/fa';
+import * as math from 'mathjs'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: 'white',
     width: '100%',
-    // width: 'auto',
-    // height: theme.spacing(500)
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -27,36 +26,46 @@ function App() {
 
   const classes = useStyles();
 
-  const [varCount, setVarCount] = useState(3);
+  const [varCount, setVarCount] = useState(2);
   const [consCount, setConsCount] = useState(3);
 
-  const [matrix, setMatrix] = useState(Array.from(Array(3 + 1), () => new Array(3 + 1)));
+  const [A, setA] = useState(Array.from(Array(3), () => new Array(2).fill(0)));
 
-  const [b, setB] = useState(Array.from(Array(3)));
+  const [c, setC] = useState(Array.from(Array(2).fill(0)));
+  const [b, setB] = useState(Array.from(Array(3).fill(0)));
 
   const [type, setType] = useState('Max');
 
+
+
+  const [solution, setSolution] = useState([]);
+
+
+
   useEffect(() => {
     if (consCount >= 0 && varCount > 0) {
-      let newMatrix = Array.from(Array(consCount + 1), () => new Array(varCount));
-      newMatrix.forEach((row, row_index) => {
-        if (matrix[row_index]) {
-          newMatrix[row_index] = matrix[row_index]
+      let newA = Array.from(Array(consCount), () => new Array(varCount).fill(0));
+      newA.forEach((row, row_index) => {
+        if (A[row_index]) {
+          newA[row_index] = A[row_index]
         }
       })
-      setMatrix(newMatrix);
+      setA(newA);
 
       let newB = Array.from(Array(consCount));
       setB(newB);
+
+      let newC = Array.from(Array(varCount));
+      setC(newC);
     }
     // eslint-disable-next-line
   }, [varCount, consCount]);
 
-  const alterMatrix = (row, col, value) => {
+  const alterA = (row, col, value) => {
     value = value === '' ? 0 : parseInt(value);
-    let newMatrix = [...matrix];
-    newMatrix[row][col] = value;
-    setMatrix(newMatrix);
+    let newA = [...A];
+    newA[row][col] = value;
+    setA(newA);
   }
 
   const alterB = (row, value) => {
@@ -64,7 +73,185 @@ function App() {
     let newB = [...b];
     newB[row] = value;
     setB(newB);
-    console.log(newB);
+  }
+
+  const alterC = (row, value) => {
+    value = value === '' ? 0 : parseInt(value);
+    let newC = [...c];
+    newC[row] = value;
+    setC(newC);
+  }
+
+
+  const iteration = (m_A, c_all, non_basic, basic, b_inv, xb) => {
+
+    let m_cn = non_basic.map(n => c_all[n])
+    let m_cb = basic.map(b => c_all[b])
+
+    let N = math.column(m_A, non_basic[0]);
+    for (var i = 1; i < non_basic.length; i++) {
+      N = math.concat(N, math.column(m_A, non_basic[i]))
+    }
+
+    let w = math.multiply(m_cb, b_inv);
+    // let zn = math.multiply(w, m_A);
+    let zn = math.multiply(w, N);
+    let zn_cn = math.subtract(zn, m_cn);
+    let entering_value = math.min(zn_cn);
+    if (entering_value >= 0) {
+      console.log('no negative values');
+      return 0;
+    }
+    let entering_index = zn_cn.toArray().indexOf(entering_value);
+    console.log(`x${entering_index + 1} enters the basis with ${entering_value}`)
+
+    // let a2 = math.column(m_A, entering_index);
+    let a2 = math.column(N, entering_index);
+    let y2 = math.multiply(b_inv, a2);
+
+    let ratios = []
+    for (var j = 0; j < y2._size[0]; j++) {
+      ratios.push(xb.get([j]) / y2.valueOf()[j][0]);
+    }
+    let leaving_index = ratios.indexOf(math.min(ratios));
+    console.log(`x${varCount + leaving_index + 1} leaves the basis`)
+
+    let n = y2.map(function (value, index, matrix) {
+      if (index[0] === leaving_index) {
+        return 1 / value;
+      } else {
+        return (-1 * value) / matrix.get([leaving_index, 0]);
+      }
+    });
+
+    let e = math.identity(consCount);
+
+    e = e.map(function (value, index, matrix) {
+      if (index[1] === leaving_index) {
+        return n.get([index[0], 0]);
+      } else {
+        return value;
+      }
+    });
+
+    b_inv = math.multiply(e, b_inv)
+
+    let entering_variable = non_basic[entering_index]
+    let leaving_variable = basic[leaving_index]
+
+    non_basic[entering_index] = leaving_variable;
+    basic[leaving_index] = entering_variable
+
+    return [non_basic, basic, b_inv, entering_variable, leaving_variable];
+  }
+
+  const calculate = () => {
+
+    setSolution(oldSolution => [...oldSolution,
+    <Typography align='center' variant='h3'>Solution</Typography>
+    ])
+    let m_A = math.matrix(A);
+    m_A = math.concat(m_A, math.identity(consCount));
+
+    let non_basic = Array.from({ length: varCount }, (_, i) => i)
+    let basic = Array.from({ length: consCount }, (_, i) => i + varCount)
+
+    let c_all = [...c].concat(Array.from(Array(consCount).fill(0)));
+
+    let xb = math.matrix(b);
+    let b_inv = math.identity(consCount);
+    let iter = 0
+    while (true) {
+      iter += 1;
+      console.log('İteration #' + (iter));
+      let text = 'Step #' + (iter);
+      let result = iteration(m_A, c_all, non_basic, basic, b_inv, xb);
+      if (result === 0) {
+        setSolution(oldSolution => [...oldSolution,
+        <div>
+          <Card >
+            <CardHeader title={text} />
+            <CardContent>
+              <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>No negative values</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Current basis is OPTIMAL</TableCell>
+                    </TableRow>
+                  </TableHead>
+                </Table>
+              </TableContainer>
+            </CardContent>
+          </Card>
+          <Typography align='center' variant='h3'>End of Solution</Typography>
+        </div>
+        ]);
+        break;
+      } else {
+        let entering_var, leaving_var;
+        [non_basic, basic, b_inv, entering_var, leaving_var] = result
+
+        xb = math.multiply(b_inv, math.matrix(b));
+        let z = math.multiply(basic.map(b => c_all[b]), xb);
+
+        let basicPrint = [...basic]
+        let nonBasicPrint = [...non_basic]
+        setSolution(oldSolution => [...oldSolution,
+        <Card >
+          <CardHeader title={text} />
+          <CardContent>
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Basic Variables</TableCell>
+                    {basicPrint.map(b => <TableCell>x{b + 1}</TableCell>)}
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Non-Basic Variables</TableCell>
+                    {nonBasicPrint.map(b => <TableCell>x{b + 1}</TableCell>)}
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Entering Variable</TableCell>
+                    <TableCell>x{entering_var + 1}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Leaving Variable</TableCell>
+                    <TableCell>x{leaving_var + 1}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Z</TableCell>
+                    <TableCell>{z}</TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </TableContainer>
+          </CardContent>
+          <CardActions>
+          </CardActions>
+        </Card>
+        ]);
+      }
+
+    }
+
+    console.log('doguhan');
+  }
+
+
+  const example = () => {
+      setVarCount(2);
+      setConsCount(3);
+      setA([
+        [1, 0],
+        [0, 2],
+        [3, 2]
+      ]);
+      setB([4, 12, 18]);
+      setC([3, 5]);
   }
 
   return (
@@ -117,8 +304,8 @@ function App() {
                     <TableCell key={i} align='center'>
                       <TextField
                         type='number'
-                        value={(matrix[0] && matrix[0][i]) || ''}
-                        onChange={e => alterMatrix(0, i, e.target.value)}
+                        value={c[i] || ''}
+                        onChange={e => alterC(i, e.target.value)}
                         InputProps={{
                           endAdornment: <InputAdornment position="end">X<sub>{i + 1}</sub></InputAdornment>,
                         }}
@@ -143,8 +330,8 @@ function App() {
                         <TableCell key={i} align='center'>
                           <TextField
                             type='number'
-                            value={(matrix[j + 1] && matrix[j + 1][i]) || ''}
-                            onChange={e => alterMatrix(j + 1, i, e.target.value)}
+                            value={(A[j] && A[j][i]) || ''}
+                            onChange={e => alterA(j, i, e.target.value)}
                             InputProps={{
                               endAdornment: <InputAdornment position="end">X{i + 1}</InputAdornment>,
                             }}
@@ -172,22 +359,40 @@ function App() {
               )
             }
             <TableRow>
-              <TableCell align='center' colSpan={(varCount * 2) + 1}>
+              <TableCell align='center' colSpan={(varCount * 2)}>
                 <Button
                   variant="contained"
                   color="primary"
+                  onClick={() => calculate()}
                   endIcon={<FaCalculator />}>
                   Calculate
+                </Button>
+              </TableCell>
+              <TableCell align='center' colSpan={1}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  onClick={() => example()}
+                  endIcon={<FaPlayCircle />}>
+                  Example
                 </Button>
               </TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
+      <br /> <br />
 
-      <div style={{ height: '1000px' }}>
-
-      </div>
+      {
+        solution.map((s, index) => {
+          return (
+            <div key={index}>
+              {s}
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
